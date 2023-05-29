00f76b7d58051e715af123e41f22f837bd9381ae
lsy3709 <lsy3709@naver.com>
2023-05-30 08:46:10 +0900

    BrowserRouter test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes , BrowserRouter} from 'react-router-dom';
 import NewsPage from './pages/NewsPage';
 
 const App = () => {
   return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element={<NewsPage />} />
       <Route path="/:category" element={<NewsPage />} />
     </Routes>
+    </BrowserRouter>
   );
 };
 
