f12d18a11f1007a24a37d58d7457b46746fb5917
lsy3709 <lsy3709@naver.com>
2023-05-30 08:52:53 +0900

    BrowserRouter test3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Route, Routes , BrowserRouter} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import NewsPage from './pages/NewsPage';
 
 const App = () => {
   return (
-    <BrowserRouter>
       <Routes>
       <Route path="/" element={<NewsPage />} />
       <Route path="/:category" element={<NewsPage />} />
     </Routes>
-    </BrowserRouter>
   );
 };
 
